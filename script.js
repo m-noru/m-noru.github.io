783249e95dc531f5c3f383a86151f2f02a6e9e96
m-noru <62436675+m-noru@users.noreply.github.com>
2020-03-25 11:02:52 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ jQuery('.menu-icon').on('click', function() {
     jQuery('.menu-container .menu').slideDown('1500');
   }
 });
+
+$(window).scroll(function (){
+   $('.fadeIn').each(function(){
+       var elemPos = $(this).offset().top,
+           scroll = $(window).scrollTop(),
+           windowHeight = $(window).height();
+         if (scroll > elemPos - windowHeight + 150){
+             $(this).addClass('scrollIn');
+           }
+       });
+   });
